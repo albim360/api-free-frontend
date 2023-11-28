318184e6def3b4b5722e7fc5b052356eedff85f8
Alberto <74522184+albim360@users.noreply.github.com>
2023-11-28 17:45:08 +0100

    FEAT try create

--- src/components/CreatePost.vue
+++ src/components/CreatePost.vue
@@ -1,75 +1,104 @@
 <template>
     <div class="form-container">
-      <h2>Crea un nuovo post</h2>
-  
-      <form @submit.prevent="createPost">
-        <div class="form-group">
-          <label class="label" for="title">Titolo:</label>
-          <input class="input" type="text" id="title" v-model="postData.title" required />
-        </div>
-  
-        <div class="form-group">
-          <label class="label" for="content">Contenuto:</label>
-          <textarea class="input" id="content" v-model="postData.content" required></textarea>
-        </div>
-  
-        <button class="button" type="submit">Crea Post</button>
-      </form>
+        <h2>Crea un nuovo post</h2>
+
+        <form @submit.prevent="createPost">
+            <div class="form-group">
+                <label class="label" for="title">Titolo:</label>
+                <input class="input" type="text" id="title" v-model="postData.title" required />
+            </div>
+
+            <div class="form-group">
+                <label class="label" for="content">Contenuto:</label>
+                <textarea class="input" id="content" v-model="postData.content" required></textarea>
+            </div>
+
+            <button class="button" type="submit" :disabled="isCreatingPost">Crea Post</button>
+        </form>
+
+        <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div>
+        <div v-if="successMessage" class="success-message">{{ successMessage }}</div>
     </div>
-  </template>
-  
-  
-  <script>
-  export default {
+</template>
+
+<script>
+export default {
     data() {
-      return {
-        postData: {
-          title: '',
-          content: '',
-        },
-      };
+        return {
+            postData: {
+                title: "",
+                content: "",
+            },
+            isCreatingPost: false,
+            errorMessage: "",
+            successMessage: "",
+        };
     },
     methods: {
-      async createPost() {
-        try {
-          const response = await this.$axios.post('http://localhost:3002/posts', this.postData);
-  
-          console.log('Post creato con successo:', response.data);
+        async createPost() {
+            this.isCreatingPost = true;
+            this.errorMessage = "";
+            this.successMessage = "";
 
-        } catch (error) {
-          console.error('Errore durante la creazione del post:', error.response.data);
+            try {
+                const response = await axios.post("http://localhost:3002/posts", this.postData).catch(error => {
+                    console.error('Errore nella richiesta HTTP:', error);
+                    throw error;  
+                });
 
+                if (response && response.data) {
+                    const post = response.data[0]; 
+                    if (post) {
+                        console.log('Post creato con successo:', post);
+                        this.successMessage = 'Post creato con successo!';
+                        this.postData = { title: '', content: '' };
+                    } else {
+                        console.error('Errore durante la creazione del post: Post non trovato nella risposta');
+                        this.errorMessage = 'Errore durante la creazione del post: Post non valido';
+                    }
+                } else {
+                    console.error('Errore durante la creazione del post: Risposta non valida');
+                    this.errorMessage = 'Errore durante la creazione del post: Risposta non valida';
+                }
+            } catch (error) {
+                if (error.response && error.response.data) {
+                    this.errorMessage = error.response.data.message || "Errore durante la creazione del post.";
+                } else {
+                    this.errorMessage = "Errore durante la creazione del post.";
+                }
+            } finally {
+                this.isCreatingPost = false;
+            }
         }
-      },
     },
-  };
-  </script>
-  
-  <style scoped>
+};
+</script>
+
+<style scoped>
 .form-container {
     max-width: 600px;
     margin: 0 auto;
-  }
-  
-  .form-group {
+}
+
+.form-group {
     margin-bottom: 20px;
-  }
-  
-  .label {
+}
+
+.label {
     display: block;
     font-size: 16px;
     margin-bottom: 8px;
-  }
-  
-  .input {
+}
+
+.input {
     width: 100%;
     padding: 10px;
     font-size: 16px;
     border: 1px solid #ccc;
     border-radius: 4px;
-  }
-  
-  .button {
+}
+
+.button {
     background-color: #007bff;
     color: #fff;
     padding: 10px 15px;
@@ -77,24 +106,21 @@
     border: none;
     border-radius: 4px;
     cursor: pointer;
-  }
-  
-  .button:hover {
+}
+
+.button:hover {
     background-color: #0056b3;
-  }
-  
-  .error-message {
+}
+
+.error-message {
     color: red;
     font-size: 14px;
     margin-top: 5px;
-  }
-  
-  .success-message {
+}
+
+.success-message {
     color: green;
     font-size: 14px;
     margin-top: 5px;
-  }
-  
-
-  </style>
-  
+}
+</style>
--- src/components/PostsList.vue
+++ src/components/PostsList.vue
@@ -5,20 +5,43 @@
       <ul class="post-list">
         <li v-for="post in posts" :key="post.id">
           <router-link :to="'/posts/' + post.slug">
-            <h3>{{ post.title }}</h3>
+            <h2>{{ post.title }}</h2>
+            <p>{{ truncateContent(post.content) }}</p>
+            <p class="date">{{ formatDate(post.createdAt) }}</p>
           </router-link>
-          <p>{{ post.content }}</p>
         </li>
       </ul>
     </div>
   </template>
   
   <script>
+  import axios from 'axios';
+  
   export default {
-    props: {
-      posts: {
-        type: Array,
-        required: true,
+    data() {
+      return {
+        posts: [],
+      };
+    },
+    mounted() {
+      // Chiamata al backend per ottenere la lista dei post
+      axios.get('http://localhost:3002/posts')
+        .then(response => {
+          this.posts = response.data;
+        })
+        .catch(error => {
+          console.error('Errore durante il recupero dei post:', error);
+        });
+    },
+    methods: {
+      truncateContent(content) {
+        // Restringi il contenuto a 100 caratteri
+        return content.length > 100 ? content.slice(0, 100) + '...' : content;
+      },
+      formatDate(dateString) {
+        // Formatta la data (assumendo che la data sia nel formato corretto)
+        const date = new Date(dateString);
+        return date.toLocaleDateString('it-IT');
       },
     },
   };
@@ -32,14 +55,21 @@
   
   .post-list li {
     margin-bottom: 20px;
+    border: 1px solid #ddd;
+    padding: 10px;
   }
   
-  .post-list h3 {
+  .post-list h2 {
     margin-bottom: 10px;
   }
   
   .post-list p {
     color: #555;
+  }
+  
+  .post-list .date {
+    color: #777;
+    font-size: 12px;
   }
   </style>
   
